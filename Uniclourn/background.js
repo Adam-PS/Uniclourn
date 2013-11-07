212a29c7ae9040bb994cddedbab57e9b70a92c48
Adam <adam@localhost.localdomain>
2013-11-06 21:43:44 -0500

    Added background.js and cloud_replace.js files

---
+++ Uniclourn/background.js
@@ -0,0 +1,4 @@
+// Calls the cloud_replace.js script on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript(null, {file: "cloud_replace.js"});
+});
---
+++ Uniclourn/could_replace.js
@@ -0,0 +1,2 @@
+// Replaces the word cloud with the word unicorn in an html document
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("cloud", "g"), "unicorn");
