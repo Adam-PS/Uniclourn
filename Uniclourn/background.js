5eb1cde2841aba270b450913e3b536ce835b5681
Adam PS <labonvie07@gmail.com>
2013-11-10 16:03:52 -0500

    Updated manifest and background.js to work without cloud_replace

--- Uniclourn/background.js
+++ Uniclourn/background.js
@@ -1,4 +1,6 @@
 // Calls the cloud_replace.js script on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(null, {file: "cloud_replace.js"});
+chrome.browserAction.onClicked.addListener(function (tab) {
+	chrome.tabs.executeScript({
+		code: 'document.body.innerHTML = document.body.innerHTML.replace(new RegExp(/cloud/gi), "unicorn")'
+	});
 });
--- Uniclourn/could_replace.js
+++
@@ -1,2 +0,0 @@
-// Replaces the word cloud with the word unicorn in an html document
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp("cloud", "g"), "unicorn");
